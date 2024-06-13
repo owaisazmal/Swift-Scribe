c0a29f412d47a6b58b3f96b9e4886575ad921b9d
owaisazmal <owaiskhan461@gmail.com>
2024-06-13 10:22:17 -0700

    Notes Slide Created!

---
+++ NotesApp/NotesSheet.swift
@@ -0,0 +1,18 @@
+//
+//  NotesSheet.swift
+//  NotesApp
+//
+//  Created by Khan, Owais on 6/13/24.
+//
+
+import SwiftUI
+
+struct NotesSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotesSheet()
+}
